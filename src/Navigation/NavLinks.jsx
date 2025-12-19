605ef349b5b93149188b99c8e1259e7fbb96f6a5
Aliaksei Leanovich <lleonovalexeyl@gmail.com>
2025-12-19 17:07:00 +0300

    feat: styles

--- src/Navigation/NavLinks.jsx
+++ src/Navigation/NavLinks.jsx
@@ -5,14 +5,34 @@ import { NavLink } from "react-router";
 const NavLinks = () => {
   const { topics } = useContext(TopicsContext);
   return (
-    <nav>
-      {topics.map((topic) => {
-        return (
-          <NavLink to={`/${topic.id}`} key={topic.id}>
-            {topic.title} ///
-          </NavLink>
-        );
-      })}
+    <nav
+      style={{
+        width: "250px",
+        backgroundColor: "#f2f2f2",
+        padding: "20px",
+        boxSizing: "border-box",
+        overflowY: "auto",
+      }}
+    >
+      <ul style={{ listStyle: "none", padding: 0, margin: 0 }}>
+        {topics.map((topic) => {
+          return (
+            <li
+              key={topic.id}
+              style={{
+                padding: "10px 15px",
+                marginBottom: "5px",
+                backgroundColor: "#fff",
+                cursor: "pointer",
+                borderRadius: "4px",
+                transition: "background-color 0.3s",
+              }}
+            >
+              <NavLink to={`/${topic.id}`}>{topic.title} ///</NavLink>
+            </li>
+          );
+        })}
+      </ul>
     </nav>
   );
 };
